211290aee87a92297b57e2c5c13cf88a22bac4eb
nurzu25-stack <nurzu25@gmail.com>
2026-04-03 19:04:50 +0800

    edit logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tahfiz Management System</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo2.png') }}">
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/main.tsx'])
 </head>
